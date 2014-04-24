92ef0719d5f133861528c3eb5a23c59dddac6fe9
Cauê Waneck <waneck@gmail.com>
2014-04-24 17:32:16 -0300

    first version

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-hx-travis
-=========
-
+# hx-travis
 Helpers (and wikis) to easily test haxe code on travis
+
+### Environment variables
+The following environment variables are sensitive by the script runtime:
+
+#### TARGET
+Defines the setup to be configured. This has exactly a 1:1 correspondence with the haxe target defined by the command-line
+
+#### TARGET_FILE
+The target file as specified by the haxe build.
+
+#### ARCH
+Defines the architecture to be used as test. The following architectures may be used:
+
+| ARCH    | Description      |  Implemented targets                                  |
+| ------  | ---------------- | ----------------------------------------------------- |
+| x86_64* | 64-bit           | all; flash is always 32-bit
+| i686    | 32-bit           | haxe, neko
+
+#### OS
+The target OS. Currently Travis support both Linux and Mac; A Windows environment may be able to be emulated in the future
+
+| OS     | Implemented targets
+| ------ | ------------------- |
+| Linux* | all
+| Mac    | -
+
+#### TOOLCHAIN
+If applicable, specifies a custom toolchain to be used for building / running. Default is always `default`
+
+|   OS    |    ARCH    |    TARGET    |  TOOLCHAIN  | Remarks
+| ------- | :--------: | :----------: | ----------- | -------
+| Linux   | `x86_64`   | cpp          | gcc* |
+| Linux   | `x86_64`   | js           | node* |
+| Linux   | `x86_64`   | js           | browser | [(1)](#toolchain_remark_browser)
+
+* <a name="toolchain_remark_browser" /> *(1)* Runs under `phantomjs`, and if `SAUCE_USERNAME` and `SAUCE_ACCESS_KEY` are defined, runs under SauceLabs as well
---
+++ setup-me.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+# setting up defaults 
+[[ $OS && ${OS-x} ]] || OS=linux
+OS=$(echo $OS | tr '[:upper:]' '[:lower:]')
+
+[[ $SETUP && ${SETUP-x} ]] || SETUP=$TARGET
+
+[[ $ARCH && ${ARCH-x} ]] || ARCH=x86_64
+case $ARCH in
+	x86_64 )
+		ARCH_BITS=64
+		NEKO_ARCH=64
+		;;
+	i686 )
+		if [ $OS = mac ]; then
+			echo "Only x86_64 architecture is support for mac"
+			exit 1
+		fi
+		ARCH_BITS=32
+		NEKO_ARCH=
+		;;
+	* )
+		echo "Unknown architecture $ARCH"
+		exit 1
+		;;
+esac
+
+[[ $TOOLCHAIN && ${TOOLCHAIN-x} ]] || TOOLCHAIN=default
+
+function retry {
+	if [ $TRAVIS && ${TRAVIS-x} ]; then
+		travis_retry $1
+	else
+		$1
+	fi
+}
+
+FIRST=0
+
+function install {
+	if [ $OS = "linux" ]; then
+		if [ $FIRST -eq 0 ]; then
+			retry sudo apt-get update
+			FIRST=1
+		fi
+		if [ $ARCH = "i686" ]; then
+			retry sudo apt-get install -qq -y $1:i386
+		else
+			retry sudo apt-get install -qq -y $1
+		fi
+	else
+		if [ $FIRST -eq 0 ]; then
+			sudo brew update
+			FIRST=1
+		fi
+		retry sudo brew install $1
+	fi
+}
+
+echo "$ARCH-$OS-$TARGET-$TOOLCHAIN-$SETUP"
+
+# compile neko
+if [ $OS = "mac" ]; then
+	echo "no prebuilt binary available; building neko"
+	retry git clone https://github.com/HaxeFoundation/neko.git ~/neko
+	cd ~neko && make && sudo make install
+else
+	retry wget -O ~/neko.tgz "http://nekovm.org/_media/neko-2.0.0-$OS$NEKO_ARCH.tar.gz"
+	tar -zxvf ~/neko.tgz 
+	cd ~/neko*
+	sudo mkdir -p /usr/lib/neko
+	sudo cp -Rf * /usr/lib/neko
+	sudo ln -s /usr/lib/neko/neko* /usr/bin
+	sudo ln -s /usr/lib/neko/lib* /usr/lib
+fi
+
+neko -version || exit 1
+echo "neko v$(neko -version)"
+
+# get haxe
+echo "getting haxe"
+retry wget -O ~/haxe.tgz "http://hxbuilds.s3-website-us-east-1.amazonaws.com/builds/haxe/$OS$ARCH_BITS/haxe_latest.tar.gz"
+cd ~
+tar -zxvf haxe.tgz
+cd haxe*
+sudo mkdir -p /usr/lib/haxe
+sudo cp -Rf * /usr/lib/haxe
+sudo ln -s /usr/lib/haxe/haxe* /usr/bin
+haxe 2>&1 | head -n 1 || exit 1
+
+# setup haxelib
+echo "setup haxelib"
+mkdir ~/haxelib && haxelib setup ~/haxelib || exit 1
+
+case $SETUP in
+	php )
+		php -v || install php5
+		;;
+	cpp )
+		install gcc-multilib
+		install g++-multilib
+		retry haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp
+		cd ~/haxelib/hxcpp/git/project
+		neko build.n
+		;;
+	java )
+		javac -version || install openjdk
+		retry haxelib git hxjava https://github.com/HaxeFoundation/hxjava
+		;;
+	cs )
+		mcs --version || install mono-mcs
+		retry haxelib git hxcs https://github.com/HaxeFoundation/hxcs
+		;;
+	flash | as3 | swf | swf9 | swf8 )
+		# TODO if the following doesn't work, uncomment either the next lines
+		retry wget http://hxbuilds.s3-website-us-east-1.amazonaws.com/unitdeps/flashplayer_11_sa_debug.i386.min.tar.xz
+		# retry wget http://hxbuilds.s3-website-us-east-1.amazonaws.com/unitdeps/flashplayer_11_sa_debug.i386.tar.gz
+		# retry wget http://fpdownload.macromedia.com/pub/flashplayer/updaters/11/flashplayer_11_sa_debug.i386.tar.gz
+		install libgd2-xpm ; install ia32-libs ; install ia32-libs-multiarch
+		[ -f /etc/init.d/xvfb ] || install xvfb
+		# retry sudo apt-get install -qq -y libgd2-xpm ia32-libs ia32-libs-multiarch
+		tar -xvf flashplayer* -C ~/
+		echo "ErrorReportingEnable=1\nTraceOutputFileEnable=1" > ~/mm.cfg
+		if [ $SETUP = "as3" ]; then
+			#TODO if the following doesn't work, uncomment either the next lines
+			retry wget -O ~/flex.tar.xz http://hxbuilds.s3-website-us-east-1.amazonaws.com/unitdeps/apache-flex-sdk-4.12.0-bin-min.tar.xz
+			#retry wget -O ~/flex.tar.gz http://hxbuilds.s3-website-us-east-1.amazonaws.com/unitdeps/apache-flex-sdk-4.12.0-bin.tar.gz
+			#retry wget -O ~/flex.tar.gz http://mirror.cc.columbia.edu/pub/software/apache/flex/4.12.0/binaries/apache-flex-sdk-4.12.0-bin.tar.gz
+			tar -xvf ~/flex.tar.gz -C ~
+			FLEXPATH=$HOME/apache-flex-sdk-4.12.0-bin/
+			export PATH=$PATH:$FLEXPATH/bin
+			mkdir -p $FLEXPATH/player/11.1
+			retry wget -nv http://download.macromedia.com/get/flashplayer/updaters/11/playerglobal11_1.swc -O "$FLEXPATH/player/11.1/playerglobal.swc"
+			echo "env.PLAYERGLOBAL_HOME=$FLEXPATH/player" > $FLEXPATH/env.properties
+			mxmlc --version || exit 1
+		fi
+		;;
+	js )
+		if [ $TOOLCHAIN = "default" ] || [ $TOOLCHAIN = "nodejs" ]; then
+			node -v || install nodejs
+		elsif [ $TOOLCHAIN = "browser" ]; then
+			phantomjs -v || install phantomjs
+		fi
+		;;
+	* )
+		;;
+esac
+
+cd $TRAVIS_BUILD_DIR
