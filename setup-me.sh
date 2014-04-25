92760fd27c9285160c467ea6dab8c7ab0a0e99ac
Cauê Waneck <waneck@gmail.com>
2014-04-24 22:06:54 -0300

    i386 tests ok

--- setup-me.sh
+++ setup-me.sh
@@ -41,7 +41,7 @@ FIRST=0
 function install {
 	if [ $OS = "linux" ]; then
 		if [ $FIRST -eq 0 ]; then
-			retry sudo apt-get update
+			retry sudo apt-get update -qq
 			FIRST=1
 		fi
 		if [ $ARCH = "i686" ] && [[ ! $1 == *:i386 ]]; then
@@ -58,21 +58,28 @@ function install {
 	fi
 }
 
+function testprog {
+	"$@" 2> /dev/null
+}
+
 echo "$ARCH-$OS-$TARGET-$TOOLCHAIN-$SETUP"
 
-git --version || install git
+git --version || sudo apt-get install -y git || install git
 
 # compile neko
 sudo rm -f /usr/bin/neko*
 sudo rm -f /usr/lib/libneko*
+sudo rm -rf /usr/lib/neko
 install libgc || install libgc1c2 || install libgc-dev
+install libpcre || install libpcre3
+install zlib1g
 if [ $OS = "mac" ]; then
 	echo "no prebuilt binary available; building neko"
 	retry git clone https://github.com/HaxeFoundation/neko.git ~/neko
 	cd ~neko && make && sudo make install
 else
 	retry wget -O ~/neko.tgz "http://nekovm.org/_media/neko-2.0.0-$OS$NEKO_ARCH.tar.gz"
-	tar -zxvf ~/neko.tgz -C ~/
+	tar -zxf ~/neko.tgz -C ~/
 	rm ~/neko.tgz
 	cd ~/neko*
 	sudo mkdir -p /usr/lib/neko
@@ -86,10 +93,11 @@ echo "neko v$(neko -version)"
 
 # get haxe
 echo "getting haxe"
+sudo rm -rf /usr/lib/haxe
 sudo rm -f /usr/bin/haxe*
 retry wget -O ~/haxe.tgz "http://hxbuilds.s3-website-us-east-1.amazonaws.com/builds/haxe/$OS$ARCH_BITS/haxe_latest.tar.gz"
 cd ~
-tar -zxvf haxe.tgz
+tar -zxf haxe.tgz
 cd haxe*
 sudo mkdir -p /usr/lib/haxe
 sudo cp -Rf * /usr/lib/haxe
@@ -102,7 +110,7 @@ mkdir -p ~/haxelib && haxelib setup ~/haxelib || exit 1
 
 case $SETUP in
 	php )
-		php -v || install php5
+		testprog php -v || sudo apt-get install -y php5 || install php5
 		;;
 	cpp )
 		install gcc-multilib
@@ -112,12 +120,12 @@ case $SETUP in
 		neko build.n || exit 1
 		;;
 	java )
-		javac -version || install openjdk || install openjdk-7-jdk || exit 1
+		testprog javac -version || install openjdk || sudo apt-get install -y openjdk-7-jdk || install openjdk-7-jdk || exit 1
 		retry haxelib git hxjava https://github.com/HaxeFoundation/hxjava
 		javac -version || exit 1
 		;;
 	cs )
-		mcs --version || install mono-mcs || exit 1
+		testprog mcs --version || sudo apt-get install -y mono-mcs || install mono-mcs || exit 1
 		retry haxelib git hxcs https://github.com/HaxeFoundation/hxcs
 		mcs --version || exit 1
 		;;
@@ -131,7 +139,7 @@ case $SETUP in
 		# retry sudo apt-get install -qq -y libgd2-xpm ia32-libs ia32-libs-multiarch
 		tar -xvf flashplayer* -C ~/
 		echo "ErrorReportingEnable=1\nTraceOutputFileEnable=1" > ~/mm.cfg
-		if [ $SETUP = "as3" ]; then
+		if [ $SETUP = "as3" ] && [ ! mxmlc --version ]; then
 			#TODO if the following doesn't work, uncomment either the next lines
 			retry wget -O ~/flex.tar.xz http://hxbuilds.s3-website-us-east-1.amazonaws.com/unitdeps/apache-flex-sdk-4.12.0-bin-min.tar.xz
 			#retry wget -O ~/flex.tar.gz http://hxbuilds.s3-website-us-east-1.amazonaws.com/unitdeps/apache-flex-sdk-4.12.0-bin.tar.gz
@@ -142,16 +150,18 @@ case $SETUP in
 			mkdir -p $FLEXPATH/player/11.1
 			retry wget -nv http://download.macromedia.com/get/flashplayer/updaters/11/playerglobal11_1.swc -O "$FLEXPATH/player/11.1/playerglobal.swc"
 			echo "env.PLAYERGLOBAL_HOME=$FLEXPATH/player" > $FLEXPATH/env.properties
-			java -version || install openjdk || install openjdk-7-jdk || exit 1
+			testprog java -version || install openjdk || install openjdk-7-jdk || exit 1
 			mxmlc --version || exit 1
 		fi
-		~/
+		~/flashplayerdebugger
 		;;
 	js )
 		if [ $TOOLCHAIN = "default" ] || [ $TOOLCHAIN = "nodejs" ]; then
-			node -v || install nodejs
+			testprog nodejs -v || node -v || install nodejs
+			nodejs -v || exit 1
 		elif [ $TOOLCHAIN = "browser" ]; then
-			phantomjs -v || install phantomjs
+			testprog phantomjs -v || install phantomjs
+			phantomjs -v || exit 1
 		fi
 		;;
 	* )
