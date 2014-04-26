37a14f2ac63728c02c7a4c93c876fef51cfa941e
Cauê Waneck <waneck@gmail.com>
2014-04-26 02:56:00 -0300

    what's wrong with node dependencies

--- runtests.sh
+++ runtests.sh
@@ -20,10 +20,13 @@ for i in "${!TARGET[@]}"; do
 						fi
 						;;
 					browser )
-						sudo npm install wd -g || exit 1
+						CURDIR=$PWD
+						cd $(dirname $0)/extra/saucelabs
+						npm install wd || exit 1
+						cd "$PWD"
 						retry curl https://gist.github.com/santiycr/5139565/raw/sauce_connect_setup.sh -L | bash
 						nekotools server &
-						node $(dirname $0)/extra/saucelabs/RunSauceLabs.js
+						node $(dirname $0)/extra/saucelabs/RunSauceLabs.js || exit 1
 						;;
 					* )
 						;;
