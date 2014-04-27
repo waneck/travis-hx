37c852708a2b2285073d92fe2b64d6f6c4af0b06
Cauê Waneck <waneck@gmail.com>
2014-04-27 19:41:13 -0300

    First appveyor test

---
+++ extra/appveyor/AppVeyor.hx
@@ -0,0 +1,125 @@
+import sys.FileSystem.*;
+import haxe.io.*;
+import sys.io.*;
+import hxcpp.StaticStd;
+import hxcpp.StaticZlib;
+using StringTools;
+
+class AppVeyor
+{
+
+	public function new()
+	{
+	}
+
+	static function main()
+	{
+		Sys.putEnv('PATH', Sys.getEnv('PATH') + ';C:\\HaxeToolkit\\neko;C:\\HaxeToolkit\\haxe');
+		Sys.putEnv('HAXEPATH', 'C:\\HaxeToolkit\\haxe');
+		Sys.putEnv('NEKO_INSTPATH', 'C:\\HaxeToolkit\\neko');
+		switch (Sys.args()[1])
+		{
+			case 'setup':
+				setup();
+		}
+	}
+
+	static function setup()
+	{
+		var home = Sys.getEnv("HOME");
+		var toolkit = "C:\\HaxeToolkit";
+		createDirectory('C:\\HaxeToolkit');
+		// download neko
+		//todo
+		// download haxe
+		trace('download haxe');
+		download('http://hxbuilds.s3-website-us-east-1.amazonaws.com/builds/haxe/windows/haxe_latest.tar.gz', '$toolkit/haxe.tar.gz');
+		untar('$toolkit/haxe.tar.gz', '$toolkit');
+		trace(readDirectory('$toolkit'));
+		for (file in readDirectory(toolkit))
+		{
+			if (file.startsWith('haxe') && isDirectory('$toolkit/$file'))
+			{
+				rename('$toolkit/$file', '$toolkit/haxe');
+			}
+		}
+
+		trace('setup haxelib');
+		// setup haxelib
+		createDirectory('$home/haxelib');
+		cmd('haxelib',['setup','$home/haxelib']);
+
+		cmd('haxe',[]); //check if it's installed correctly
+		trace('configuring target');
+		for (target in Sys.getEnv("TARGET").split(" "))
+		{
+			switch target
+			{
+				case 'cpp':
+					cmd('haxelib', ['git','hxcpp','https://github.com/HaxeFoundation/hxcpp'],3);
+					cd('$home/haxelib/hxcpp/git/project');
+					cmd('neko', ['build.n']);
+				case 'cs':
+					cmd('haxelib', ['git','hxcs','https://github.com/HaxeFoundation/hxcs'],3);
+				case 'java':
+					cmd('haxelib', ['git','hxjava','https://github.com/HaxeFoundation/hxjava'],3);
+					cmd('javac',['--version']);
+			}
+		}
+	}
+
+	static function download(url:String,target:String,?retry=3)
+	{
+		do
+		{
+			var req = new haxe.Http(url);
+			var err = null;
+			req.onError = function(msg) err = msg;
+			var out = sys.io.File.write(target);
+			req.customRequest(false, out);
+			out.close();
+			if (err != null && retry <= 0)
+				throw 'Cannot download $url : $err';
+		} while (retry-- > 0);
+	}
+
+	static function untar(filename:String, target:String)
+	{
+		var file = sys.io.File.read(filename);
+		var gz = new format.gz.Reader(file);
+		var out = new BytesOutput();
+		gz.readHeader();
+		gz.readData(out);
+		var tar = new format.tar.Reader(new BytesInput(out.getBytes()));
+		for (entry in tar.read())
+		{
+			createDirectory(target + '/' + Path.directory(entry.fileName));
+			if (entry.data != null)
+			{
+				File.saveBytes( target + '/' + entry.fileName, entry.data );
+			}
+		}
+	}
+
+	static function cd(dir:String)
+	{
+		Sys.setCwd(dir);
+	}
+
+	static function cmd(cmd:String,args:Array<String>,retry=0,throwOnError=true)
+	{
+		var ret = -1;
+		do
+		{
+			ret = Sys.command(cmd,args);
+			if (ret == 0)
+			{
+				return 0;
+			}
+		} while(retry-- > 0);
+		if (throwOnError)
+			throw '$cmd response: $ret';
+		return ret;
+	}
+
+}
