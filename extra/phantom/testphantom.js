ac9662eee8b9012be8e05ca327272b53e7ea6427
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-10-09 20:06:55 -0600

    moved scope

--- extra/phantom/testphantom.js
+++ extra/phantom/testphantom.js
@@ -8,28 +8,26 @@ page.open('http://localhost:2000/unit-js.html', function(status) {
 	var delay = 200,
 			retries = 100; // 20 seconds
 
-	function getContent() {
-		return page.evaluate(function () {
-			return document.getElementById('haxe:trace').textContent.trim();
+		page.evaluate(function () {
+			function getContent() {
+				return document.getElementById('haxe:trace').textContent.trim();
+			}
+			function poll() {
+				var content = getContent();
+				if(content) {
+					console.log(content);
+					return phantom.exit(0);
+				}
+
+				if(--retries === 0) {
+					// no content
+					console.log("status error: phantom timeout");
+					return phantom.exit(1);
+				}
+
+				setTimeout(poll, delay);
+			}
+
+			poll();
 		});
-	}
-
-	function poll() {
-		var content = getContent();
-		if(content) {
-			console.log(content);
-			return phantom.exit(0);
-		}
-
-		if(--retries === 0) {
-			// no content
-			console.log("status error: phantom timeout");
-			return phantom.exit(1);
-		}
-
-		console.log('polling ...');
-		setTimeout(poll, delay);
-	}
-
-	poll();
 });
