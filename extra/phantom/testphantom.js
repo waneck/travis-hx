8fbac2c6fa5e04373baa480b054fa319a83d1028
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-10-09 19:58:53 -0600

    improved

--- extra/phantom/testphantom.js
+++ extra/phantom/testphantom.js
@@ -1,9 +1,8 @@
 var page = require('webpage').create();
 page.open('http://localhost:2000/unit-js.html', function(status) {
-	if (status != "success")
-	{
+	if (status != "success") {
 		console.log("status error: " + status);
-		phantom.exit(1);
+		return phantom.exit(1);
 	}
 
 	var delay = 200,
@@ -18,17 +17,17 @@ page.open('http://localhost:2000/unit-js.html', function(status) {
 	function poll() {
 		var content = getContent();
 		if(content) {
-			console.log(ua);
-			phantom.exit(0);
-			return;
+			console.log(content);
+			return phantom.exit(0);
 		}
 
-		if(retries-- === 0) {
+		if(--retries === 0) {
 			// no content
-			phantom.exit(1);
-			return;
+			console.log("status error: phantom timeout");
+			return phantom.exit(1);
 		}
 
+		console.log('polling ...');
 		setTimeout(poll, delay);
 	}
 
