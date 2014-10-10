82a9208a8fdfb838fe06170f2cffc2d19ca573df
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-10-09 20:13:48 -0600

    reversed

--- extra/phantom/testphantom.js
+++ extra/phantom/testphantom.js
@@ -8,26 +8,27 @@ page.open('http://localhost:2000/unit-js.html', function(status) {
 	var delay = 200,
 			retries = 100; // 20 seconds
 
-		page.evaluate(function () {
-			function getContent() {
-				return document.getElementById('haxe:trace').textContent.trim();
-			}
-			function poll() {
-				var content = getContent();
-				if(content) {
-					console.log(content);
-					return phantom.exit(0);
-				}
-
-				if(--retries === 0) {
-					// no content
-					console.log("status error: phantom timeout");
-					return phantom.exit(1);
-				}
-
-				setTimeout(poll, delay);
-			}
-
-			poll();
+	function getContent() {
+		return page.evaluate(function () {
+			return document.getElementById('haxe:trace').textContent.trim();
 		});
+	}
+
+	function poll() {
+		var content = getContent();
+		if(content) {
+			console.log(content);
+			return phantom.exit(0);
+		}
+
+		if(--retries === 0) {
+			// no content
+			console.log("status error: phantom timeout");
+			return phantom.exit(1);
+		}
+
+		setTimeout(poll, delay);
+	}
+
+	poll();
 });
