8961dfeb08f33543b319f5e2c27cc99f0acdf698
Dmitri Granetchi <system.grand@gmail.com>
2015-06-24 17:08:32 +0300

    temporary fix java compilation error

--- tests/fail/BuddyTests.hx
+++ tests/fail/BuddyTests.hx
@@ -1,8 +1,20 @@
+import buddy.reporting.TravisHxReporter;
 import buddy.*;
 using buddy.Should;
 
+// Temporary fix https://github.com/HaxeFoundation/haxe/issues/4286
+#if java
+class BuddyTests {
+	static public function main() {
+		var reporter = new TravisHxReporter();
+		var runner = new SuitesRunner([new FailureTest()], reporter);
+		runner.run().then(function(_) { Sys.exit(runner.statusCode()); });
+	}
+}
+#else
 // compile with `haxe -lib buddy -main Buddy`
 class BuddyTests implements buddy.Buddy<[FailureTest]> {}
+#end
 
 class FailureTest extends buddy.BuddySuite
 {
